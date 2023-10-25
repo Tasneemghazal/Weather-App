cc47b00ea8f97e2d27ab13743740b352919d4829
Tasneem <tasneemgazal5@gmail.com>
2023-10-25 19:28:58 +0300

    update temp

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
+const inputCity=document.querySelector('.search input');
+const searchBtn =document.querySelector('.search  button ');
 const apiKey = "b76e2377b523dd5f31ffce570a07d0c0";
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore&appid=${apiKey}`;
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
-  const response = await fetch(apiUrl);
+async function checkWeather(city) {
+  const response = await fetch(apiUrl+city+`&appid=${apiKey}`);
   var data = await response.json();
 
   console.log(data);
+  document.querySelector('.city').innerHTML = data.name;
+  document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector('.humidity').innerHTML = data.main.humidity +"%";
+  document.querySelector('.wind').innerHTML = data.wind.speed + " km/hour";
 }
-checkWeather();
+searchBtn.addEventListener('click', ()=>{
+    checkWeather(inputCity.value);
+})
+
+
